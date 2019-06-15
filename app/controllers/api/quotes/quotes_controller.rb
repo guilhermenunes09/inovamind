fc152b9d15af9c18d2a879468daa08b4b5f48ef7
Guilherme <guilhermenunes09@protonmail.com>
2019-06-15 05:09:10 +0000

    Initial API structure

---
+++ app/controllers/api/quotes/quotes_controller.rb
@@ -0,0 +1,15 @@
+class Api::Quotes::QuotesController < Api::Quotes::ApiController
+    def show
+        puts params[:search_tag]
+        render json: {
+            quotes: [
+                {
+                quote: "frase",
+                author: "nome do autor",
+                author_about: "link para o perfil do autor",
+                tags: ["tag1", "tag2"]
+                }
+            ]
+        }.to_json
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :quotes do
+      resources :quotes
+      get ':search_tag' => "quotes#show"
+    end
+  end
 end

50ee93ace4b041bc820e3e420e4d9f8825f91c85
Guilherme <guilhermenunes09@protonmail.com>
2019-06-15 16:40:21 +0000

    Crawling

--- app/controllers/api/quotes/quotes_controller.rb
+++ app/controllers/api/quotes/quotes_controller.rb
@@ -1,5 +1,30 @@
 class Api::Quotes::QuotesController < Api::Quotes::ApiController
     def show
+        require 'open-uri'
+        require 'nokogiri'
+        require 'json'
+
+        url = 'http://quotes.toscrape.com/'
+        html = open(url)
+
+        doc = Nokogiri::HTML(html)
+        
+        quotes = doc.css("div.quote")
+        bridges = []
+
+        #json = JSON.pretty_generate(bridges)
+        # File.open("data.json", 'w') { 
+        #     |file| file.write(json) 
+        # }
+
+        puts "Debugging<<<<<<<<<<<<<<"
+        quotes.each_with_index do |div, index|
+            quote_test = div.css("span.text")[0].text
+            author = div.css("small.author").text
+            author_about_link = div.css("a")[0]['href']
+            keywords = div.css("meta.keywords")[0]['content']
+        end
+
         puts params[:search_tag]
         render json: "teste"
     end

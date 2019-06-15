914615f9de4ba9f9ce5b1969815a94c0bd88752b
Guilherme <guilhermenunes09@protonmail.com>
2019-06-15 15:48:17 +0000

    Mongodb Setup

--- app/controllers/api/quotes/quotes_controller.rb
+++ app/controllers/api/quotes/quotes_controller.rb
@@ -1,15 +1,19 @@
 class Api::Quotes::QuotesController < Api::Quotes::ApiController
     def show
         puts params[:search_tag]
-        render json: {
-            quotes: [
-                {
-                quote: "frase",
-                author: "nome do autor",
-                author_about: "link para o perfil do autor",
-                tags: ["tag1", "tag2"]
-                }
-            ]
-        }.to_json
+        render json: "teste"
     end
 end
+
+#http://quotes.toscrape.com/.
+
+# render json: {
+#     quotes: [
+#         {
+#         quote: "frase",
+#         author: "nome do autor",
+#         author_about: "link para o perfil do autor",
+#         tags: ["tag1", "tag2"]
+#         }
+#     ]
+# }.to_json
